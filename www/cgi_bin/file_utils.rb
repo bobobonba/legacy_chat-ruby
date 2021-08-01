817811c8a0875f03a560b06ebfdfa244527cefd9
BONBA <torikabuto1018@gmail.com>
2021-08-01 16:27:47 +0900

    tailを実装した

--- www/cgi_bin/file_utils.rb
+++ www/cgi_bin/file_utils.rb
@@ -1,13 +1,13 @@
 def tail(file_path, read_line=10)
-  current_bite = 0
+  current_byte = 0
   file = File.open(file_path.to_s)
-  file.seek(current_bite, IO::SEEK_END)
+  file.seek(current_byte, IO::SEEK_END)
   indention_count = read_line + 1
 
   until indention_count.zero?
-    current_bite -= 1
+    current_byte -= 1
     begin
-      file.seek(current_bite, IO::SEEK_END)
+      file.seek(current_byte, IO::SEEK_END)
       indention_count -= 1 if file.getc == "\n"
     rescue
       file.seek(0)
